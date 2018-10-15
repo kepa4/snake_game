640177bbb7e10bb712dc9d837170be151bedf6a9
Keenan <Kpautler@henselphelps.com>
2018-10-15 11:01:10 -0600

    apple generated randomely at start and dissappears when snake eats it and is regenerated in a different spot

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,70 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext('2d');
 
-var interval = 50;
+var interval = 100;
 
 function snake(x, y) {
     this.x = x;
     this.y = y;
+    var appleX = Math.floor(Math.random() * 30) * 20;
+    var appleY = Math.floor(Math.random() * 30) * 20;
     var numberToAddByX = 0; 
-    var numberToAddByY = -10;
+    var numberToAddByY = -20;
 
+    function apple(){
+        context.fillRect(appleX, appleY, 20, 20);
+    } 
+    
     setInterval(function() {
         document.onkeydown = function(key) {
-            if (key.keyCode === 87 && numberToAddByY !== 10){ 
-                numberToAddByY = -10;
+            if (key.keyCode === 87 && numberToAddByY !== 20){ 
+                numberToAddByY = -20;
                 numberToAddByX = 0;
             }
-            if (key.keyCode === 83 && numberToAddByY !== -10){ 
-                numberToAddByY = 10;
+            if (key.keyCode === 83 && numberToAddByY !== -20){ 
+                numberToAddByY = 20;
                 numberToAddByX = 0;
             }
-            if (key.keyCode === 68 && numberToAddByX !== -10){ 
+            if (key.keyCode === 68 && numberToAddByX !== -20){ 
                 numberToAddByY = 0;
-                numberToAddByX = 10;
+                numberToAddByX = 20;
             }
-            if (key.keyCode === 65 && numberToAddByX !== 10){ 
+            if (key.keyCode === 65 && numberToAddByX !== 20){ 
                 numberToAddByY = 0;
-                numberToAddByX = -10;
+                numberToAddByX = -20;
             }
         }
         context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle="red";
+        apple();
+        context.fillStyle='black';
         context.fillRect(x, y, 20, 20);
         x += numberToAddByX;
         y += numberToAddByY;         
-        if (x === 500) {
+        if (x >= 600) {
             context.clearRect(0, 0, canvas.width, canvas.height); 
-            x = 250;
-            y = 250;
+            x = 300;
+            y = 300;
         }
-        if ( x === -10) {
+        if ( x <= -20) {
             context.clearRect(0, 0, canvas.width, canvas.height); 
-            x = 250;
-            y = 250;
+            x = 300;
+            y = 300;
         }
-        if (y === 500) {
+        if (y >= 600) {
             context.clearRect(0, 0, canvas.width, canvas.height); 
-            x = 250;
-            y = 250;
+            x = 300;
+            y = 300;
         }
-        if (y === -10) {
+        if (y <= -20) {
             context.clearRect(0, 0, canvas.width, canvas.height); 
-            x = 250;
-            y = 250;
+            x = 300;
+            y = 300;
+        }
+        if (x === appleX  && y === appleY){
+            appleY = Math.floor(Math.random() * 30) * 20;
+            appleX = Math.floor(Math.random() * 30) * 20;
+            
         }
     }, interval);
     
